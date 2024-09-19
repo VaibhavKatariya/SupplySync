b54ad1e053c960b3a4ef1c6fcef72274a079a9e8
Vaibhav Katariya <86615124+VaibhavKatariya@users.noreply.github.com>
2024-09-20 01:36:15 +0530

    Fix 404 page

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -6,7 +6,7 @@ function Page() {
         <>
             <div className={styles.main}>
                 <h1 className={styles.h1}>404 - Page Not Found</h1>
-                <p className={styles.p}>Sorry, the page you're looking for doesn't exist.</p>
+                <p className={styles.p}>Sorry, the page you&apos;re looking for doesn&apos;t exist.</p>
                 <Link legacyBehavior href="/"><a className={styles.a}>Go Back Home</a></Link>
             </div>
         </>
