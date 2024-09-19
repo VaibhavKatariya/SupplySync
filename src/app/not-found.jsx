1212ac254e7135f2231c9b63fff3c596d8e01b54
Vaibhav Katariya <86615124+VaibhavKatariya@users.noreply.github.com>
2024-09-20 01:29:09 +0530

    Add 404 page

---
+++ src/app/not-found.jsx
@@ -0,0 +1,16 @@
+import Link from "next/link"
+import styles from "./not-found.module.css"
+
+function Page() {
+    return (
+        <>
+            <div className={styles.main}>
+                <h1 className={styles.h1}>404 - Page Not Found</h1>
+                <p className={styles.p}>Sorry, the page you're looking for doesn't exist.</p>
+                <Link legacyBehavior href="/"><a className={styles.a}>Go Back Home</a></Link>
+            </div>
+        </>
+    )
+}
+
+export default Page
